2752567ed7fcef58595b0094f346484bf2850e3e
hyoneekim <suhyunkim@Mac.wlan>
2025-09-22 21:28:40 +0300

    w6-bb-pp iteration 4 added

---
+++ week6-bepp-starter/routers/tourRouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const requireAuth = require('../middleware/requireAuth');
+const {
+  getAllTours,
+  getTourById,
+  createTour,
+  updateTour,
+  deleteTour,
+} = require("../controllers/tourControllers");
+
+router.use(requireAuth);
+
+router.get("/", getAllTours);
+router.post("/", createTour);
+router.get("/:tourId", getTourById);
+router.put("/:tourId", updateTour);
+router.delete("/:tourId", deleteTour);
+
+module.exports = router;
